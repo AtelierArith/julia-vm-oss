d70548a85fa913f098e91f0f61690eedb68ffd7d
sohamb117 <sohamb117@gmail.com>
2026-08-18 23:43:13 -0700

    Add Wasm AoT IR lowering
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ subset_julia_vm/src/aot/codegen/wasm/lower/control.rs
@@ -0,0 +1,153 @@
+use crate::aot::ir::{AotExpr, AotStmt, BasicBlock, Instruction, IrFunction, Terminator, VarRef};
+use crate::aot::types::StaticType;
+use crate::aot::AotResult;
+
+use super::super::types::unsupported;
+use super::ops::ensure_type;
+use super::Lowerer;
+
+impl Lowerer<'_> {
+    pub(super) fn stmt(&mut self, stmt: &AotStmt, function: &mut IrFunction) -> AotResult<()> {
+        match stmt {
+            AotStmt::Let {
+                name, ty, value, ..
+            } => self.bind(name, ty, value, function),
+            AotStmt::Assign {
+                target: AotExpr::Var { name, ty },
+                value,
+            } => self.bind(name, ty, value, function),
+            AotStmt::Assign {
+                target:
+                    AotExpr::Index {
+                        array,
+                        indices,
+                        is_tuple: false,
+                        ..
+                    },
+                value,
+            } if indices.len() == 1 => {
+                let array = self.expr(array, function)?;
+                let index = self.expr(&indices[0], function)?;
+                let value = self.expr(value, function)?;
+                self.current_block_mut(function)?
+                    .push(Instruction::SetIndex {
+                        array,
+                        index,
+                        value,
+                    });
+                Ok(())
+            }
+            AotStmt::Expr(expr) | AotStmt::ValueCarrier(expr) => {
+                self.expr(expr, function).map(|_| ())
+            }
+            AotStmt::Return(value) => {
+                let value = value
+                    .as_ref()
+                    .map(|expr| self.expr(expr, function))
+                    .transpose()?;
+                self.current_block_mut(function)?
+                    .set_terminator(Terminator::Return(value));
+                Ok(())
+            }
+            AotStmt::If {
+                condition,
+                then_branch,
+                else_branch,
+            } => self.if_stmt(
+                condition,
+                then_branch,
+                else_branch.as_deref().unwrap_or(&[]),
+                function,
+            ),
+            AotStmt::While { condition, body } => self.while_stmt(condition, body, function),
+            _ => Err(unsupported(format!(
+                "Wasm AoT cannot lower statement `{stmt:?}`"
+            ))),
+        }
+    }
+
+    fn bind(
+        &mut self,
+        name: &str,
+        ty: &StaticType,
+        value: &AotExpr,
+        function: &mut IrFunction,
+    ) -> AotResult<()> {
+        ensure_type(ty)?;
+        let src = self.expr(value, function)?;
+        let dest = VarRef::new(name.to_string(), ty.clone());
+        self.current_block_mut(function)?.push(Instruction::Copy {
+            dest: dest.clone(),
+            src,
+        });
+        self.vars.insert(name.to_string(), dest);
+        Ok(())
+    }
+
+    fn if_stmt(
+        &mut self,
+        condition: &AotExpr,
+        then_stmts: &[AotStmt],
+        else_stmts: &[AotStmt],
+        function: &mut IrFunction,
+    ) -> AotResult<()> {
+        let cond = self.expr(condition, function)?;
+        let then_label = self.label("if_then");
+        let else_label = self.label("if_else");
+        let join_label = self.label("if_join");
+        self.current_block_mut(function)?
+            .set_terminator(Terminator::Branch {
+                cond,
+                then_block: then_label.clone(),
+                else_block: else_label.clone(),
+            });
+        function.add_block(BasicBlock::new(then_label.clone()));
+        self.current = then_label;
+        self.stmts(then_stmts, function)?;
+        let then_falls_through = self.current_block(function)?.terminator.is_none();
+        if then_falls_through {
+            self.jump_if_open(&join_label, function)?;
+        }
+        function.add_block(BasicBlock::new(else_label.clone()));
+        self.current = else_label;
+        self.stmts(else_stmts, function)?;
+        let else_falls_through = self.current_block(function)?.terminator.is_none();
+        if else_falls_through {
+            self.jump_if_open(&join_label, function)?;
+        }
+        if then_falls_through || else_falls_through {
+            function.add_block(BasicBlock::new(join_label.clone()));
+            self.current = join_label;
+        }
+        Ok(())
+    }
+
+    fn while_stmt(
+        &mut self,
+        condition: &AotExpr,
+        body: &[AotStmt],
+        function: &mut IrFunction,
+    ) -> AotResult<()> {
+        let cond_label = self.label("while_cond");
+        let body_label = self.label("while_body");
+        let exit_label = self.label("while_exit");
+        self.current_block_mut(function)?
+            .set_terminator(Terminator::Jump(cond_label.clone()));
+        function.add_block(BasicBlock::new(cond_label.clone()));
+        self.current = cond_label.clone();
+        let cond = self.expr(condition, function)?;
+        self.current_block_mut(function)?
+            .set_terminator(Terminator::Branch {
+                cond,
+                then_block: body_label.clone(),
+                else_block: exit_label.clone(),
+            });
+        function.add_block(BasicBlock::new(body_label.clone()));
+        self.current = body_label;
+        self.stmts(body, function)?;
+        self.jump_if_open(&cond_label, function)?;
+        function.add_block(BasicBlock::new(exit_label.clone()));
+        self.current = exit_label;
+        Ok(())
+    }
+}
---
+++ subset_julia_vm/src/aot/codegen/wasm/lower/expr.rs
@@ -0,0 +1,151 @@
+use crate::aot::ir::{AotBuiltinOp, AotExpr, ConstValue, Instruction, IrFunction, VarRef};
+use crate::aot::types::StaticType;
+use crate::aot::AotResult;
+
+use super::super::types::unsupported;
+use super::ops::{ensure_type, map_binop, map_unary};
+use super::Lowerer;
+
+impl Lowerer<'_> {
+    pub(super) fn expr(&mut self, expr: &AotExpr, function: &mut IrFunction) -> AotResult<VarRef> {
+        match expr {
+            AotExpr::LitI64(value) => {
+                self.constant(ConstValue::Int64(*value), StaticType::I64, function)
+            }
+            AotExpr::LitI32(value) => {
+                self.constant(ConstValue::Int32(*value), StaticType::I32, function)
+            }
+            AotExpr::LitF64(value) => {
+                self.constant(ConstValue::Float64(*value), StaticType::F64, function)
+            }
+            AotExpr::LitBool(value) => {
+                self.constant(ConstValue::Bool(*value), StaticType::Bool, function)
+            }
+            AotExpr::Var { name, .. } => self.vars.get(name).cloned().ok_or_else(|| {
+                unsupported(format!("Wasm AoT could not resolve variable `{name}`"))
+            }),
+            AotExpr::BinOpStatic {
+                op,
+                left,
+                right,
+                result_ty,
+            } => {
+                ensure_type(result_ty)?;
+                let left = self.expr(left, function)?;
+                let right = self.expr(right, function)?;
+                let dest = self.temporary(result_ty.clone());
+                self.current_block_mut(function)?.push(Instruction::BinOp {
+                    dest: dest.clone(),
+                    op: map_binop(*op)?,
+                    left,
+                    right,
+                });
+                Ok(dest)
+            }
+            AotExpr::UnaryOp {
+                op,
+                operand,
+                result_ty,
+            } => {
+                let operand = self.expr(operand, function)?;
+                if matches!(op, crate::aot::ir::AotUnaryOp::Pos) {
+                    return Ok(operand);
+                }
+                let dest = self.temporary(result_ty.clone());
+                self.current_block_mut(function)?
+                    .push(Instruction::UnaryOp {
+                        dest: dest.clone(),
+                        op: map_unary(*op)?,
+                        operand,
+                    });
+                Ok(dest)
+            }
+            AotExpr::CallStatic {
+                function: callee,
+                args,
+                return_ty,
+                ..
+            } => {
+                let args = args
+                    .iter()
+                    .map(|arg| self.expr(arg, function))
+                    .collect::<AotResult<Vec<_>>>()?;
+                let dest =
+                    (*return_ty != StaticType::Nothing).then(|| self.temporary(return_ty.clone()));
+                self.current_block_mut(function)?.push(Instruction::Call {
+                    dest: dest.clone(),
+                    func: callee.clone(),
+                    args,
+                });
+                dest.ok_or_else(|| unsupported("Nothing-returning call cannot be used as a value"))
+            }
+            AotExpr::CallBuiltin {
+                builtin: AotBuiltinOp::Length,
+                args,
+                return_ty,
+            } if args.len() == 1 => {
+                let args = vec![self.expr(&args[0], function)?];
+                let dest = self.temporary(return_ty.clone());
+                self.current_block_mut(function)?.push(Instruction::Call {
+                    dest: Some(dest.clone()),
+                    func: "__sjulia_u8_len".to_string(),
+                    args,
+                });
+                Ok(dest)
+            }
+            AotExpr::Index {
+                array,
+                indices,
+                elem_ty,
+                is_tuple: false,
+            } if indices.len() == 1 => {
+                let array = self.expr(array, function)?;
+                let index = self.expr(&indices[0], function)?;
+                let dest = self.temporary(elem_ty.clone());
+                self.current_block_mut(function)?
+                    .push(Instruction::GetIndex {
+                        dest: dest.clone(),
+                        array,
+                        index,
+                    });
+                Ok(dest)
+            }
+            AotExpr::Convert { value, target_ty }
+                if matches!(
+                    target_ty,
+                    StaticType::U8
+                        | StaticType::I64
+                        | StaticType::I32
+                        | StaticType::F64
+                        | StaticType::Bool
+                ) =>
+            {
+                let src = self.expr(value, function)?;
+                let dest = self.temporary(target_ty.clone());
+                self.current_block_mut(function)?.push(Instruction::Copy {
+                    dest: dest.clone(),
+                    src,
+                });
+                Ok(dest)
+            }
+            _ => Err(unsupported(format!(
+                "Wasm AoT cannot lower expression `{expr:?}`"
+            ))),
+        }
+    }
+
+    fn constant(
+        &mut self,
+        value: ConstValue,
+        ty: StaticType,
+        function: &mut IrFunction,
+    ) -> AotResult<VarRef> {
+        let dest = self.temporary(ty);
+        self.current_block_mut(function)?
+            .push(Instruction::LoadConst {
+                dest: dest.clone(),
+                value,
+            });
+        Ok(dest)
+    }
+}
